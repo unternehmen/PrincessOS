2be4d41a95e9c9d4cfd1489394ddd5ea94e90a44
unternehmen <unternehmen@users.noreply.github.com>
2017-09-26 15:48:49 -0400

    Added more timekeeping and encapsulated ProcessImage

--- ProcessImage.java
+++ ProcessImage.java
@@ -22,10 +22,19 @@ public class ProcessImage
      * the first I/O burst requires five bubble sorts, the second
      * CPU burst requires two bubble sorts, and so on.
      */
-    public String code;
+    private String code;
 
     /** The time when the process was created */
-    private long realCreationTime;
+    private long creationTime;
+
+    /** The time the process took to reach its first I/O wait */
+    private long responseTime;
+
+    /** The time the process took to terminate */
+    private long latency;
+
+    /** Whether the process has waited yet */
+    private boolean hasWaited;
 
     /**
      * Create a new ProcessImage.
@@ -42,9 +51,10 @@ public class ProcessImage
                         int arrivalTime,
                         String code)
     {
-        realCreationTime = System.currentTimeMillis();
+        creationTime = System.currentTimeMillis();
         this.processControlBlock = new PCB(id, priority, arrivalTime);
         this.code = code;
+        hasWaited = false;
     }
 
     /**
@@ -60,5 +70,57 @@ public class ProcessImage
         }
 
         return (int)(code.charAt(index) - '0');
+    }
+
+    /**
+     * Return the number of instructions in the process' code.
+     *
+     * @return the number of instructions
+     */
+    public int getCodeLength()
+    {
+        return code.length();
+    }
+
+    /**
+     * Return the program counter.
+     *
+     * @return  the program counter
+     */
+    public int getProgramCounter()
+    {
+        return processControlBlack.programCounter;
+    }
+
+    /**
+     * Set the program counter.
+     *
+     * @param programCounter  the new program counter
+     */
+    public void setProgramCounter(int programCounter)
+    {
+        processControlBlack.programCounter = programCounter;
+    }
+
+    /**
+     * Set the state of the process.
+     *
+     * You should run this whenever the state changes so that the
+     * process can record latency, response time, and other statistics
+     * related to its life cycle.
+     */
+    public void setState(PCB.ProcessState state)
+    {
+        if (state == PCB.ProcessState.WAITING) {
+            if (!hasWaited) {
+                long currentTime = System.currentTimeMillis();
+                responseTime = currentTime - creationTime;
+                hasWaited = true;
+            }
+        } else if (state == PCB.ProcessState.TERMINATED) {
+            latency = System.currentTimeMillis() - creationTime;
+        }
+
+        processControlBlock.state = state;
     }
 }
